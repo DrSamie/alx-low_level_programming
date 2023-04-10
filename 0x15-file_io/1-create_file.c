ef21779d126b8be415173006802e1e20200cc004
DrSamie <omoko.samuel@gmail.com>
2023-04-10 14:43:08 -0700

    final update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,18 +7,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int a, b, len = 0;
+	int a, b, c = 0;
 
 	if (filename == NULL)
 	return (-1);
 	if (text_content != NULL)
 	{
-	for (len = 0; text_content[len];)
-	len++;
+	for (c = 0; text_content[c];)
+	c++;
 	}
 
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	b = write(a, text_content, len);
+	b = write(a, text_content, c);
 
 	if (a == -1 || b == -1)
 	return (-1);
